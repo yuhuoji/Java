912f88ae1ff62f288962ca3448903dfaab4e7650
yuhuoji <yuhuoji@outlook.com>
2024-07-08 18:01:59 +0800

    update

---
+++ src/main/java/com/leetcode/editor/cn/LC377CombinationSumIv.java
@@ -0,0 +1,23 @@
+package com.leetcode.editor.cn;
+
+import com.leetcode.helper.*;
+
+// 377 组合总和 Ⅳ
+public class LC377CombinationSumIv {
+    public static void main(String[] args) {
+        System.out.println("Leetcode " + 377);
+        Solution solution = new LC377CombinationSumIv().new Solution();
+
+    }
+
+    // TODO @date 2024-07-08
+    // 从头开始爬楼梯，爬到楼顶，和为target
+// leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int combinationSum4(int[] nums, int target) {
+
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/com/leetcode/editor/cn/LC746MinCostClimbingStairs.java
+++ src/main/java/com/leetcode/editor/cn/LC746MinCostClimbingStairs.java
@@ -15,6 +15,29 @@ public class LC746MinCostClimbingStairs {
     // 求跳到n位置的花费最小
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        public int minCostClimbingStairs(int[] cost) {
+            int n = cost.length;
+            int f0 = 0, f1 = 0, f2 = 0;
+            for (int i = 2; i <= n; ++i) {
+                f2 = Math.min(f1 + cost[i - 1], f0 + cost[i - 2]);
+                f0 = f1;
+                f1 = f2;
+
+            }
+            return f1;
+        }
+
+        public int minCostClimbingStairs1(int[] cost) {
+            int n = cost.length;
+            int[] f = new int[n + 1];
+            for (int i = 2; i <= n; ++i) {
+                f[i] = Math.min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+            }
+            return f[n];
+        }
+    }
+
+    class Solution1 {
         int[] cost;
         int k = 2;
 
