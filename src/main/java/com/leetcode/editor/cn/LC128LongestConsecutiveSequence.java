027b722c61f2f8d51f289b4aebea67078dc251eb
yuhuoji <yuhuoji@outlook.com>
2025-03-07 17:34:33 +0800

    update

--- src/main/java/com/leetcode/editor/cn/LC128LongestConsecutiveSequence.java
+++ src/main/java/com/leetcode/editor/cn/LC128LongestConsecutiveSequence.java
@@ -12,12 +12,90 @@ public class LC128LongestConsecutiveSequence {
 
     }
 
-    // REVIEW @date 2024-07-22 并查集（哈希表，带size）
+    // REVIEW @date 2025-03-07 并查集模版
+    // 并查集（哈希表实现，带size）
     // 1.哈希表
     // 2.并查集
     // leetcode submit region begin(Prohibit modification and deletion)
 
+    // 并查集
     class Solution {
+        private final Map<Integer, Integer> father = new HashMap<>();
+        private final Map<Integer, Integer> size = new HashMap<>();
+
+        // 路径优化
+        private int find(int i) {
+            if (i != father.get(i)) {
+                father.put(i, find(father.get(i)));
+            }
+            return father.get(i);
+        }
+
+        private boolean isSameSet(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        // 路径优化 小挂大
+        private void union(int x, int y) {
+            int fx = find(x);
+            int fy = find(y);
+            if (fx != fy) {
+                if (size.get(fx) < size.get(fy)) {
+                    father.put(fx, fy);
+                    size.put(fy, size.get(fx) + size.get(fy));
+                } else {
+                    father.put(fy, fx);
+                    size.put(fx, size.get(fx) + size.get(fy));
+                }
+            }
+        }
+
+        public int longestConsecutive(int[] nums) {
+            int n = nums.length;
+            if (n == 0) {
+                return 0;
+            }
+            // 初始化
+            for (int x : nums) {
+                father.put(x, x);
+                size.put(x, 1);
+            }
+            // union
+            for (int x : nums) {
+                if (father.containsKey(x + 1)) {
+                    union(x, x + 1);
+                }
+            }
+            return Collections.max(size.values());
+        }
+    }
+
+    // 哈希表
+    // 012..56..7..9 分段统计，求最长的长度
+    class Solution1 {
+        public int longestConsecutive(int[] nums) {
+            Set<Integer> st = new HashSet<>();
+            for (int x : nums) {
+                st.add(x);
+            }
+            int ans = 0;
+            for (int x : st) {
+                if (st.contains(x - 1)) {
+                    continue;
+                }
+                int curNum = x;
+                int curLen = 1;
+                while (st.contains(curNum + 1)) {
+                    curNum++;
+                    curLen++;
+                }
+                ans = Math.max(ans, curLen);
+            }
+            return ans;
+        }
+    }
+
+    class Solution2 {
         // 并查集 最大的集合容量
         private final Map<Integer, Integer> father = new HashMap<>();
         private final Map<Integer, Integer> size = new HashMap<>();
@@ -60,30 +138,7 @@ public class LC128LongestConsecutiveSequence {
         }
     }
 
-    // 哈希表
-    // 012..56..7..9 分段统计，求最长的长度
-    class Solution1 {
-        public int longestConsecutive(int[] nums) {
-            Set<Integer> st = new HashSet<>();
-            for (int x : nums) {
-                st.add(x);
-            }
-            int ans = 0;
-            for (int x : st) {
-                if (st.contains(x - 1)) {
-                    continue;
-                }
-                int curNum = x;
-                int curLen = 1;
-                while (st.contains(curNum + 1)) {
-                    curNum++;
-                    curLen++;
-                }
-                ans = Math.max(ans, curLen);
-            }
-            return ans;
-        }
-    }
+
 // leetcode submit region end(Prohibit modification and deletion)
 
 }
--- src/main/java/com/leetcode/editor/cn/LC283MoveZeroes.java
+++ src/main/java/com/leetcode/editor/cn/LC283MoveZeroes.java
@@ -11,11 +11,39 @@ public class LC283MoveZeroes {
         Solution solution = new LC283MoveZeroes().new Solution();
 
     }
-
+// TODO @date 2025-03-07
     // REVIEW @date 2024-07-23 快排解法
 
     // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        // 双指针 没有0｜都是0｜还没到的
+        public void moveZeroes(int[] nums) {
+            int n = nums.length;
+            int i = 0, j = 0;
+            while (j < n) {
+                while (i < n && nums[i] != 0) {
+                    i++;
+                }
+                if (i > n) {
+                    break;
+                }
+                // nums[i]=0
+                while (j < n && nums[j] == 0) {
+                    j++;
+                }
+                if (j >= n) {
+                    break;
+                }
+                // nums[j]!=0
+
+                // 交换ij
+                nums[i] = nums[j];
+                nums[j] = 0;
+            }
+        }
+    }
+
+    class Solution1 {
         // 快排思想 不等于0的放在左边，等于0的放在右边，ij之间的是0
         public void moveZeroes(int[] nums) {
             int n = nums.length;
